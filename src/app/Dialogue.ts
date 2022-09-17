7fc3996b0cfcd403f288f9813e2370bfa61c9842
Papenmeier, Andrea <andrea.papenmeier@gesis.org>
2022-09-17 12:05:04 +0200

    added scrolling

--- src/app/Dialogue.ts
+++ src/app/Dialogue.ts
@@ -3,8 +3,8 @@
 export var chatbotMessages: any = {
   "greeting": {
     "start": [
-      new dialogueTurn("bot", "Hey! Let me search for a laptop for you :)"),
-      new dialogueTurn("bot", "I just have a few questions first."),
+      new dialogueTurn("bot", "Hey! We have 865 Laptops. I can help you find laptops that work for you."),
+      new dialogueTurn("bot", "I just have a couple of questions:"),
     ],
   },
   "purpose": {
@@ -14,15 +14,15 @@ export var chatbotMessages: any = {
     "noKeyfacts": [
       [new dialogueTurn("bot","Sorry, can you explain again?", false, "none", "purpose"),
         new dialogueTurn("bot","I did not get that, can you rephrase?", false, "none", "purpose"),
-        new dialogueTurn("bot","I did not get that, what do you mean?", false, "none", "purpose"),
-        new dialogueTurn("bot","I am not sure what you mean, can you explain again please", false, "none", "purpose"),
+        new dialogueTurn("bot","I did not get that. What do you mean?", false, "none", "purpose"),
+        new dialogueTurn("bot","I am not sure what you mean, can you explain again please?", false, "none", "purpose"),
       ],
-      [new dialogueTurn("bot","Many users do things like document editing, browsing, gaming, and so on.", true, "open", "purpose")
+      [new dialogueTurn("bot","Many people use their laptop for things like document editing, browsing the internet, gaming, and so on. What about you?", true, "open", "purpose")
       ]
     ],
     "notImportant": [
-      new dialogueTurn("bot","What I understand from your answer is that you do not have specific tasks.", false, "none", "purpose"),
-      new dialogueTurn("bot","In those cases, I usually recommend to start with a basic laptop. Is that fine with you? (we also have advanced or gaming laptops)", true, "yesno", "purpose"),
+      new dialogueTurn("bot","What I understand from your answer is that you do not have specific tasks. That's no problem.", false, "none", "purpose"),
+      new dialogueTurn("bot","In those cases, I usually recommend to start with a basic laptop. Is that fine with you? (We also have advanced or gaming laptops)", true, "yesno", "purpose"),
     ],
     "no": [
       new dialogueTurn("bot","Okay, can you explain again what tasks you need your laptop for?", true, "open", "purpose"),
@@ -35,16 +35,16 @@ export var chatbotMessages: any = {
     "noKeyfacts": [
       [new dialogueTurn("bot","Sorry, can you explain again?", false, "none", "price"),
         new dialogueTurn("bot","I did not get that, can you rephrase?", false, "none", "price"),
-        new dialogueTurn("bot","I did not get that, what do you mean?", false, "none", "price"),
-        new dialogueTurn("bot","I am not sure what you mean, can you explain again please", false, "none", "price"),
+        new dialogueTurn("bot","I did not get that. What do you mean?", false, "none", "price"),
+        new dialogueTurn("bot","I am not sure what you mean, can you explain again please?", false, "none", "price"),
       ],
-      [new dialogueTurn("bot","Most laptops cost around 500", true, "open", "price"),
+      [new dialogueTurn("bot","Most laptops cost around 500 pounds.", true, "open", "price"),
         new dialogueTurn("bot","Many people need a laptop around 500 pounds.", true, "open", "price"),
       ],
     ],
     "notImportant": [
       new dialogueTurn("bot","What I understand is that price is not too important for you.", false, "none", "price"),
-      new dialogueTurn("bot","In that case, I am going to leave that open for now and make sure that the laptops meet the other requirements. Is that fine with you?", true, "yesno", "price"),
+      new dialogueTurn("bot","In that case, I am going to leave that open for now and make sure that the laptops meet your other requirements. Is that fine with you?", true, "yesno", "price"),
     ],
     "no": [
       new dialogueTurn("bot","Okay, then what price point were you thinking of?", true, "open", "price"),
@@ -57,8 +57,8 @@ export var chatbotMessages: any = {
     "noKeyfacts": [
       [new dialogueTurn("bot","Sorry, can you explain again?", false, "none", "battery"),
         new dialogueTurn("bot","I did not get that, can you rephrase?", false, "none", "battery"),
-        new dialogueTurn("bot","I did not get that, what do you mean?", false, "none", "battery"),
-        new dialogueTurn("bot","I am not sure what you mean, can you explain again please", false, "none", "battery"),
+        new dialogueTurn("bot","I did not get that. What do you mean?", false, "none", "battery"),
+        new dialogueTurn("bot","I am not sure what you mean, can you explain again please?", false, "none", "battery"),
       ],
       [new dialogueTurn("bot","Most laptop batteries last for 8 hours.", true, "open", "battery"),
         new dialogueTurn("bot","Many users want their laptop to last at least 8 hours.", true, "open", "battery"),
@@ -79,8 +79,8 @@ export var chatbotMessages: any = {
     "noKeyfacts": [
       [new dialogueTurn("bot","Sorry, can you explain again?", false, "none", "storage"),
         new dialogueTurn("bot","I did not get that, can you please rephrase?", false, "none", "storage"),
-        new dialogueTurn("bot","I did not get that, what do you mean?", false, "none", "storage"),
-        new dialogueTurn("bot","I am not sure what you mean, can you explain again please", false, "none", "storage"),
+        new dialogueTurn("bot","I did not get that. What do you mean?", false, "none", "storage"),
+        new dialogueTurn("bot","I am not sure what you mean, can you explain again please?", false, "none", "storage"),
       ],
       [new dialogueTurn("bot","Most laptops have a storage between 500 and 1000 GB.", true, "open", "storage"),
         new dialogueTurn("bot","Many users want the storage to be between 500 GB and 1000GB.", true, "open", "storage"),
