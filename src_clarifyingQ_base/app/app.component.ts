3b390c38ffdb9f24e5cca7dcaf40b7aa9b2d1483
Papenmeier, Andrea <andrea.papenmeier@gesis.org>
2023-04-17 17:04:42 +0200

    cleaned MWE

--- src_clarifyingQ_base/app/app.component.ts
+++ src_clarifyingQ_base/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
-import { chatbotMessages } from './Dialogue';
-import { DialogueTurn } from './DialogueTurns';
 
 
 @Component({
