ea57d9eeded079e788b4654ed7101cf244dde799
kevin11444 <93664364+kevin11444@users.noreply.github.com>
2023-10-19 14:38:49 +0200

    Update app.module.ts

--- src_chatbot_kevin/app/app.module.ts
+++ src_chatbot_kevin/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { StatusbarComponent } from '../statusbar/statusbar.component';
 import { ScrollButtonComponent } from '../scroll-button/scroll-button.component';
+import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
+import { DialogContentComponent } from 'src/dialog-content/dialog-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatusbarComponent,
-    ScrollButtonComponent
+    ScrollButtonComponent,
+    DialogBoxComponent,
+    DialogContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    NgbModalModule,
     FormsModule,
     HttpClientModule
   ],
