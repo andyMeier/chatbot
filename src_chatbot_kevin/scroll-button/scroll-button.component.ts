17389c495252b259092c64bced031f9a6511dcc1
kevin11444 <93664364+kevin11444@users.noreply.github.com>
2023-10-20 20:21:04 +0200

    Update scroll-button.component.ts

--- src_chatbot_kevin/scroll-button/scroll-button.component.ts
+++ src_chatbot_kevin/scroll-button/scroll-button.component.ts
@@ -1,25 +1,48 @@
-import { Component, Input } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
 
 @Component({
-  selector: 'app-dialog-box',
-  templateUrl: './dialog-box.component.html',
-  styleUrls: ['./dialog-box.component.css']
+  selector: 'app-scroll-button',
+  templateUrl: './scroll-button.component.html',
+  styleUrls: ['./scroll-button.component.css']
 })
-export class DialogBoxComponent {
-  @Input() dialogTitle: string = "";
+export class ScrollButtonComponent {
+  isButtonFixed: boolean = true;
+  originalPosition: { top: string, right: string };
 
-  constructor(public activeModal: NgbActiveModal) {}
-
-  handleYes() {
-    // Handle 'Yes' button click
-    // You can perform actions or close the modal if needed
-    this.activeModal.close('Yes');
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private modalService: NgbModal
+  ) {
+    this.originalPosition = {
+      top: this.el.nativeElement.style.top,
+      right: this.el.nativeElement.style.right
+    };
   }
 
-  handleNo() {
-    // Handle 'No' button click
-    // You can perform actions or close the modal if needed
-    this.activeModal.close('No');
+  @HostListener('window:scroll', [])
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+  const scrollOffset = window.scrollY;
+  const offsetTop = 180; // Set the desired offset from the top
+  const offsetRight = 20; // Set the desired offset from the right
+
+  if (scrollOffset > 200) {
+    this.renderer.setStyle(this.el.nativeElement, 'position', 'fixed');
+    this.renderer.setStyle(this.el.nativeElement, 'top', `${offsetTop}px`);
+    this.renderer.setStyle(this.el.nativeElement, 'right', `${offsetRight}px`);
+    this.isButtonFixed = true;
+  } else if (!this.isButtonFixed && scrollOffset <= 200) {
+    this.renderer.setStyle(this.el.nativeElement, 'position', 'static');
+    this.renderer.setStyle(this.el.nativeElement, 'top', this.originalPosition.top);
+    this.renderer.setStyle(this.el.nativeElement, 'right', this.originalPosition.right);
+    this.isButtonFixed = false;
+  }
+  }
+
+  openDialog() {
+    const modalRef = this.modalService.open(DialogBoxComponent);
   }
 }
