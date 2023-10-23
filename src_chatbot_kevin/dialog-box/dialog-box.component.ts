038dee56bc1933373edb4b1f274a04584241de2e
kevin11444 <93664364+kevin11444@users.noreply.github.com>
2023-10-23 11:45:54 +0200

    Update dialog-box.component.ts

--- src_chatbot_kevin/dialog-box/dialog-box.component.ts
+++ src_chatbot_kevin/dialog-box/dialog-box.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { DialogCommunicationService } from '../dialog-communication.service'; // Update this import path
+import { DialogCommunicationService } from '../dialog-communication.service';
 
 @Component({
   selector: 'app-dialog-box',
   templateUrl: './dialog-box.component.html',
   styleUrls: ['./dialog-box.component.css']
 })
+
 export class DialogBoxComponent {
   @Input() dialogTitle: string = "";
+  showFeedback: boolean = false; // Add this line
+  questionText: string = "Is this a laptop you would buy?"; // Add this line
+  inputPlaceholder: string = "Please describe why you would not buy the laptop"; // Add this line
 
   constructor(
     public activeModal: NgbActiveModal,
@@ -23,7 +27,16 @@ export class DialogBoxComponent {
 
   handleNo() {
     // Handle 'No' button click
-    this.dialogCommunicationService.sendResponse('No');
+    this.showFeedback = true; // Add this line
+    this.questionText = "Please describe why you would not buy the laptop."; // Add this line
+    this.inputPlaceholder = "Please type here..."; // Add this line
+  }
+
+  handleSubmit(feedback: string) {
+    // Handle feedback submission
+    console.log(feedback);
+    // Send 'feedback' to the DialogCommunicationService
+    this.dialogCommunicationService.sendResponse(feedback);
     this.activeModal.close('No');
   }
 }
