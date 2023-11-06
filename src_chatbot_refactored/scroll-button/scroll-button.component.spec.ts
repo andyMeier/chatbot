d4e040697ecc5243c64114bbbdcad64edd505ab2
kevin11444 <93664364+kevin11444@users.noreply.github.com>
2023-11-06 16:34:42 +0100

    Scroll button TS update

--- src_chatbot_refactored/scroll-button/scroll-button.component.spec.ts
+++ src_chatbot_refactored/scroll-button/scroll-button.component.spec.ts
@@ -1,23 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, HostListener } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
 
-import { ScrollButtonComponent } from './scroll-button.component';
+@Component({
+  selector: 'app-scroll-button',
+  templateUrl: './scroll-button.component.html',
+  styleUrls: ['./scroll-button.component.css']
+})
+export class ScrollButtonComponent {
+  isButtonFixed: boolean = false; // Set to false initially to hide the button
 
-describe('ScrollButtonComponent', () => {
-  let component: ScrollButtonComponent;
-  let fixture: ComponentFixture<ScrollButtonComponent>;
+  constructor(private modalService: NgbModal) {}
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ScrollButtonComponent ]
-    })
-    .compileComponents();
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollOffset = window.scrollY;
 
-    fixture = TestBed.createComponent(ScrollButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    if (scrollOffset > 500) {
+      this.isButtonFixed = true;
+    }
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  openDialog() {
+    const modalRef = this.modalService.open(DialogBoxComponent);
+  }
+}
